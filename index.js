72a734abeb7d42bdd8e6b20a6b4d630e62e31143
+ <+>
2024-08-22 19:27:17 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,5 +1,80 @@
 /* Your Code Here */
+function createEmployeeRecord(employeeData) {
+    const [firstName, familyName, title, payPerHour] = employeeData;
 
+    const employeeRecord = {
+        firstName,
+        familyName,
+        title,
+        payPerHour,
+        timeInEvents: [],
+        timeOutEvents: []
+    };
+
+    return employeeRecord;
+}
+
+function createEmployeeRecords(arrayOfArrays) {
+    return arrayOfArrays.map(createEmployeeRecord);
+}
+
+function createTimeInEvent(dateStamp) {
+
+    let [date, time] = dateStamp.split(' ');
+    let hour = parseInt(time);
+
+
+    let timeInEvent = {
+        type: "TimeIn",
+        hour: parseInt(hour),
+        date: date
+    };
+
+    this.timeInEvents = this.timeInEvents || [];
+    this.timeInEvents.push(timeInEvent);
+
+    return this;
+}
+
+function createTimeOutEvent(dateStamp) {
+
+    let [date, time] = dateStamp.split(' ');
+    let hour = parseInt(time);
+
+
+    let timeOutEvent = {
+        type: "TimeOut",
+        hour: parseInt(hour),
+        date: date
+    };
+
+    this.timeOutEvents = this.timeOutEvents || [];
+    this.timeOutEvents.push(timeOutEvent);
+
+    return this;
+}
+
+function hoursWorkedOnDate(employeeRecord, date) {
+    const timeInEvent = employeeRecord.timeInEvents.find(event => event.date === date);
+    const timeOutEvent = employeeRecord.timeOutEvents.find(event => event.date === date);
+
+
+    let hoursWorked = parseInt(timeOutEvent.hour) - parseInt(timeInEvent.hour);
+
+
+    if (hoursWorked < 0) {
+        hoursWorked += 24;
+    }
+
+    return hoursWorked / 100;
+}
+
+function wagesEarnedOnDate(employeeRecord, date, payPerHour) {
+    const hoursWorked = hoursWorkedOnDate(employeeRecord, date);
+    const wagesEarned = hoursWorked * employeeRecord.payPerHour;
+  
+    return wagesEarned;
+}
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
@@ -21,3 +96,22 @@ const allWagesFor = function () {
     return payable
 }
 
+function findEmployeeByFirstName(srcArray, firstName) {
+    for (let i = 0; i < srcArray.length; i++) {
+      if (srcArray[i].firstName === firstName) {
+        return srcArray[i];
+      }
+    }
+    return undefined;
+  }
+
+function calculatePayroll(employeeRecords) {
+    let totalPay = 0;
+  
+    for (const employeeRecord of employeeRecords) {
+      const wages = allWagesFor(employeeRecord);
+      totalPay += wages;
+    }
+  
+    return totalPay;
+}
